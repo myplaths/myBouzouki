61b47d9a68cbeff74d11c81d576a00754c7574d2
myplaths <manolhslplaths46@gmail.com>
2020-01-07 14:53:20 +0000

    updated03

--- Assets/gameManager.cs
+++ Assets/gameManager.cs
@@ -7,15 +7,17 @@ using UnityEngine.UI;
 
 public class gameManager : MonoBehaviour
 {
+    List<string> GetList = new List<string>();
+    public List<string> myList = new List<string>();
+
     public InputField inputNotes;
     public GameObject[] noteDot;
-    public List<string> myList = new List<string>();
-    public string LastCharacterofMyList;
+    public string LastCharacterofGetList;
     public float notesDelay;
 
     public int counterx = 0;
-    public string outputName;
-    public string withoutSpace;
+    
+
 
     void Update()
     {
@@ -23,22 +25,42 @@ public class gameManager : MonoBehaviour
         //make inputnotes To UpperCase
         InputfieldToUpperCase();
 
-    
+
         if (Input.GetKeyDown("v"))
         {
            
-                
+
         }
 
 
 
     }
-
-    public void RemoveEmptySpace()
+    void AddInmyList()
     {
+        for (int i = 0; i < ReturnNotes().Length; i++)
+        {
+            myList.Add(ReturnNotes()[i].ToString());
+            
+        }
         for (int i = 0; i < myList.Count; i++)
         {
-            outputName += myList[i];
+           
+            if (myList[i].Contains("#"))
+            {
+                myList[i - 1] = myList[i-1] + myList[i];
+                myList.RemoveAt(i);
+            }
+        }
+    }
+
+
+    public string ReturnNotes()
+    {
+         string outputName = "";
+        string withoutSpace = ""; 
+        for (int i = 0; i < GetList.Count; i++)
+        {
+            outputName += GetList[i];
         }
 
         foreach (var letter in outputName)
@@ -48,11 +70,9 @@ public class gameManager : MonoBehaviour
                 continue;
             }
             withoutSpace += letter;
-        }
-
-        Debug.Log("its called");
-        string text = inputNotes.text;
-        //outputName.Trim();
+        }        
+       
+        return withoutSpace;
     }
 
     void InputfieldToUpperCase()
@@ -61,40 +81,27 @@ public class gameManager : MonoBehaviour
         if (text != inputNotes.text.ToUpper())
         {
             inputNotes.text = inputNotes.text.ToUpper();
-            
+
         }
     }
 
     //this method adds in a list each letter
     public void AddInListTheInputNotesEachSeparateLetter(string x)
     {
-        
+
         for (int i = 0; i < x.Length; i++)
         {
-            myList.Add(x[i].ToString());
-
-            if (x[i] == '#')
-            {               
-                
-                myList[i-1]=(x[i-1].ToString() + x[i]);
-                if (x[i] == '#')
-                {
-
-                    myList[i] = "";
-
-                }
-                
-            }
+            GetList.Add(x[i].ToString());          
 
         }
 
     }
-    
-   
+
+
 
     IEnumerator SetActiveWithString(List<string> texts)
     {
-       
+
         for (int i = 0; i < texts.Count; i++)
         {
             if (texts[i] == "C")
@@ -108,11 +115,11 @@ public class gameManager : MonoBehaviour
                 {
                     Debug.Log("counterx is " + counterx);
                 }
-               
+
                 noteDot[0].SetActive(true);
                 yield return new WaitForSeconds(notesDelay);
                 noteDot[0].SetActive(false);
-               
+
 
             }
             else if (texts[i] == "D")
@@ -120,42 +127,55 @@ public class gameManager : MonoBehaviour
                 noteDot[1].SetActive(true);
                 yield return new WaitForSeconds(notesDelay);
                 noteDot[1].SetActive(false);
-            }else if (texts[i] == "E")
+            }
+            else if (texts[i] == "E")
             {
                 noteDot[2].SetActive(true);
                 yield return new WaitForSeconds(notesDelay);
                 noteDot[2].SetActive(false);
-            }else if (texts[i] == "F")
+            }
+            else if (texts[i] == "F")
             {
                 noteDot[3].SetActive(true);
                 yield return new WaitForSeconds(notesDelay);
                 noteDot[3].SetActive(false);
-            }else if (texts[i] == "G")
+            }
+            else if (texts[i] == "G")
             {
                 noteDot[4].SetActive(true);
                 yield return new WaitForSeconds(notesDelay);
                 noteDot[4].SetActive(false);
-            }else if (texts[i] == "A")
+            }if (texts[i] == "G#")
+            {
+                noteDot[10].SetActive(true);
+                yield return new WaitForSeconds(notesDelay);
+                noteDot[10].SetActive(false);
+            }
+            else if (texts[i] == "A")
             {
                 noteDot[5].SetActive(true);
                 yield return new WaitForSeconds(notesDelay);
                 noteDot[5].SetActive(false);
-            }else if (texts[i] == "B")
+            }
+            else if (texts[i] == "B")
             {
                 noteDot[6].SetActive(true);
                 yield return new WaitForSeconds(notesDelay);
                 noteDot[6].SetActive(false);
-            }else if (texts[i] == "A#")
+            }
+            else if (texts[i] == "A#")
             {
                 noteDot[9].SetActive(true);
                 yield return new WaitForSeconds(notesDelay);
                 noteDot[9].SetActive(false);
-            }else if (texts[i] == "C#")
+            }
+            else if (texts[i] == "C#")
             {
                 noteDot[7].SetActive(true);
                 yield return new WaitForSeconds(notesDelay);
                 noteDot[7].SetActive(false);
-            }else if (texts[i] == "F#")
+            }
+            else if (texts[i] == "F#")
             {
                 noteDot[8].SetActive(true);
                 yield return new WaitForSeconds(notesDelay);
@@ -170,18 +190,16 @@ public class gameManager : MonoBehaviour
     public void DoSomethingWithNotes()
     {
         AddInListTheInputNotesEachSeparateLetter(inputNotes.text);
-       
-            StartCoroutine(SetActiveWithString(myList));
-        RemoveEmptySpace();
-
-
-
+        AddInmyList();
+        StartCoroutine(SetActiveWithString(myList));
+        
     }
 
     public void Clear()
     {
+        GetList.Clear();
         myList.Clear();
         counterx = 0;
-       
+
     }
 }
